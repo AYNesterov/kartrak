8d0871b04fff7ae31920aa3a9206c111779fe30b
AYNesterov <39973776+AYNesterov@users.noreply.github.com>
2019-07-15 18:25:26 +0200

    Add files via upload

---
+++ ArduinoSensors/ArduinoSensors.ino
@@ -0,0 +1,44 @@
+#define trigPin1 10
+#define echoPin1 13
+#define trigPin2 9
+#define echoPin2 12
+
+long duration, distance, RightSensor,BackSensor,FrontSensor,LeftSensor;
+
+void setup()
+{
+Serial.begin (9600);
+pinMode(trigPin1, OUTPUT);
+pinMode(echoPin1, INPUT);
+pinMode(trigPin2, OUTPUT);
+pinMode(echoPin2, INPUT);
+}
+
+void loop() {
+SonarSensor(trigPin1, echoPin1);
+RightSensor = distance;
+SonarSensor(trigPin2, echoPin2);
+LeftSensor = distance;
+
+
+Serial.print(LeftSensor);
+Serial.print(",");
+
+
+Serial.println(RightSensor);
+delay(200);
+
+
+}
+
+void SonarSensor(int trigPin,int echoPin)
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance = (duration/2) / 29.1;
+
+}
